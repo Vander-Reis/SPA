13b5f6535d983b4d09aa83df70a909380adceaeb
Vander Reis <vanderreis2017@outlook.com>
2022-05-16 16:50:44 -0300

    first-commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import { Router }  from "./routes.js"
+
+const router = new Router()
+
+router.add("/", "/pages/home.html")
+router.add("/universe", "/pages/universe.html")
+router.add("/explorer", "/pages/explorer.html")
+router.add(404, "/pages/404.html")
+
+router.handle()
+
+window.PopStateEvent = () => router.handle()
+window.route = () => router.route()
